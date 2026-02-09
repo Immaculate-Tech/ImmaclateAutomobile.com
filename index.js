a702068f0278c300a3c9dba3a8c5b81346c580f0
Immaculate-Tech <adekoyaaromolaran@gmail.com>
2026-02-09 13:43:39 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+function opn(){
+    document.getElementById('on').style.display='none'
+    document.getElementById('off').style.display='block'
+    document.getElementById('menu-bar').style.display='block'
+}
+function clo(){
+    document.getElementById('off').style.display='none'
+    document.getElementById('on').style.display='block'
+    document.getElementById('menu-bar').style.display='none'
+}
